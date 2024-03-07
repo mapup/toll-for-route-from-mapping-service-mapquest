ef2e07d2612b98daaf7f29ae78ff5e6e898783a9
Abhinav Saini <abhinavs@mapup.ai>
2024-03-08 02:18:17 +0530

    refactor: javascript

--- javascript/index.js
+++ javascript/index.js
@@ -8,18 +8,32 @@ const TOLLGURU_API_KEY = process.env.TOLLGURU_API_KEY;
 const TOLLGURU_API_URL = "https://apis.tollguru.com/toll/v2";
 const POLYLINE_ENDPOINT = "complete-polyline-from-mapping-service";
 
-const source = 'Dallas, TX'
-const destination = 'New York, NY';
+const source = "Philadelphia, PA";
+const destination = "New York, NY";
+
+// Explore https://tollguru.com/toll-api-docs to get the best of all the parameters that tollguru has to offer
+const requestParameters = {
+  vehicle: {
+    type: "2AxlesAuto",
+  },
+  // Visit https://en.wikipedia.org/wiki/Unix_time to know the time format
+  departure_time: "2021-01-05T09:46:08Z",
+};
 
 const url = `${MAPQUEST_API_URL}?key=${MAPQUEST_API_KEY}&from=${source}&to=${destination}&fullShape=true`;
 
-const head = arr => arr[0];
+const head = (arr) => arr[0];
 const flatten = (arr, x) => arr.concat(x);
 
 // JSON path "$..shapePoints"
-const getPoints = body => body.route.shape.shapePoints.reduce((arr, x, i, origArr) => i % 2 == 0 ? [...arr, [origArr[i], origArr[i + 1]]] : arr, [])
+const getPoints = (body) =>
+  body.route.shape.shapePoints.reduce(
+    (arr, x, i, origArr) =>
+      i % 2 == 0 ? [...arr, [origArr[i], origArr[i + 1]]] : arr,
+    []
+  );
 
-const getPolyline = body => polyline.encode(getPoints(JSON.parse(body)));
+const getPolyline = (body) => polyline.encode(getPoints(JSON.parse(body)));
 
 const getRoute = (cb) => request.get(url, cb);
 
@@ -31,7 +45,6 @@ const getRoute = (cb) => request.get(url, cb);
 const tollguruUrl = `${TOLLGURU_API_URL}/${POLYLINE_ENDPOINT}`;
 
 const handleRoute = (e, r, body) => {
-
   console.log(body);
   const _polyline = getPolyline(body);
   console.log(_polyline);
@@ -40,16 +53,20 @@ const handleRoute = (e, r, body) => {
     {
       url: tollguruUrl,
       headers: {
-        'content-type': 'application/json',
-        'x-api-key': TOLLGURU_API_KEY
+        "content-type": "application/json",
+        "x-api-key": TOLLGURU_API_KEY,
       },
-      body: JSON.stringify({ source: "mapquest", polyline: _polyline })
+      body: JSON.stringify({
+        source: "mapquest",
+        polyline: _polyline,
+        ...requestParameters,
+      }),
     },
     (e, r, body) => {
       console.log(e);
-      console.log(body)
+      console.log(body);
     }
-  )
-}
+  );
+};
 
 getRoute(handleRoute);
